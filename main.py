26f78c2ea01cb06922e62e7fcff0def34528e205
zarafshan-za <zarjumand.za@gmail.com>
2025-10-25 12:58:00 +0500

    Update main.py
    Added comments.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# main.py
-# Launcher for the Edge Detection Explorer application.
+# Imports
 
 import sys
 from PyQt5.QtWidgets import QApplication
 from gui import EdgeExplorer
 
 def main():
-    app = QApplication(sys.argv)
-    win = EdgeExplorer(display_size=480)
+    app = QApplication(sys.argv) # creates application instance
+    win = EdgeExplorer(display_size=480) # creates main window
     
     win.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec_()) # starts event loop
 
+# ensures main only runs when the script is directly executed
 if __name__ == "__main__":
     main()
